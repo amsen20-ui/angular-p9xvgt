0921c5225f6a77b0a07448817cbdb4041541a61d
amsen20-ui <amita.sen@wipro.com>
2019-11-08 14:57:51 +0530

    1 file modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { Hello2Component } from './hello2/hello2.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, Hello2Component ],
+  declarations: [ AppComponent, HelloComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
